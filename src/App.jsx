412b8971921cab2dc41cca0f36b36c3278cf8cc0
Owaish <owaish3301@gmail.com>
2025-10-18 21:06:30 +0530

    feat: implement routing with react-router in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { Routes, Route } from "react-router";
 import Dashboard from "./pages/Dashboard";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
 
 function App(){
   return(
-    <div>
-      <Dashboard />
-    </div>
+    <Routes>
+      <Route path='/home' element={ <Dashboard /> } />
+      <Route path='/signin' element={ <SignIn /> } />
+      <Route path="/signup" element={ <SignUp /> } />
+      <Route path="/reset-password" element={ <ForgotPassword /> } />
+    </Routes>
   )
 }
 
